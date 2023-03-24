afb9c4e74e990d84a631af4631ae0d55026c5f19
Fred Tu <90625393+STRAGALAY@users.noreply.github.com>
2023-03-24 12:46:31 -0400

    Create mystery_box.js

---
+++ JS-basics/FT-JSB/mystery_box.js
@@ -0,0 +1,12 @@
+/**
+ * Features:
+ * 1. @var MYSTERY_BOX: Object with a box 4x4 array and boxIterativePrinter method
+ *      - @var
+ *      - @var boxIterativePrinter: ...
+ */
+
+const MYSTERY_BOX = {};
+console.log(MYSTERY_BOX.box);
+
+function boxIterativePrinter() {}
+console.log(boxIterativePrinter(MYSTERY_BOX));
